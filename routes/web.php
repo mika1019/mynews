0eb611f6a27fcf57ec5acd4048ba9527dd456f0c
mika <smile.happiness8484@gmail.com>
2019-10-25 10:12:19 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+Route::get('XXX', 'AAAController@bbb');
