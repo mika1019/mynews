c4bc09dadcbb60b6201ace495ec1abbc5b7db1e4
mika <smile.happiness8484@gmail.com>
2019-11-10 05:53:17 +0000

    プロフィール編集画面の作成

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -14,15 +14,7 @@ class ProfileController extends Controller
     }
 
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+    
     public function create(Request $request)
   {
 
@@ -33,6 +25,7 @@ class ProfileController extends Controller
       $profile = new Profile;
       $form = $request->all();
 
+        // dd($form['_token']);
       // フォームから送信されてきた_tokenを削除する
       unset($form['_token']);
 
@@ -41,5 +34,27 @@ class ProfileController extends Controller
       $profile->save();
 
       return redirect('admin/profile/create');
+  }
+  public function edit(Request $request)
+  {
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // Profile Modelからデータを取得する
+      $news = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile/');
   }
 }
